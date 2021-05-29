4c97952c12d587c1690b5af54bc245cf52fc3bf6
Chris Gruel <chris.gruel@armory.io>
2021-05-29 06:27:20 -0400

    talend vault injector

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express();
 
 var PORT = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
-    res.status(200).send('Hello world');
+    res.status(200).send('Hello world!!!!');
+});
+
+app.get('/accesskey', (req, res) => {
+    const file = "/opt/talend/secrets/secrets.properties"
+    const secret = fs.readFileSync(file, 'utf8');
+    res.status(200).send(`Boom, secret is : ${secret}`);
 });
 
 app.listen(PORT, function() {
-    console.log('Server is running on PORT:',PORT);
+    console.log('Server is running on PORT:', PORT);
 });
