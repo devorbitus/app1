5d421dbceca7e92e7f1cbe2e30d900aef23abbd5
Chris Gruel <chris.gruel@armory.io>
2021-05-31 07:51:36 -0400

    format results better

--- server.js
+++ server.js
@@ -6,13 +6,14 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.get('/', function(_req, res) {
-    res.status(200).send('Hello world!!!!');
+    res.status(200).send('Hello world!!!!!!');
 });
 
 app.get('/accesskey', (_req, res) => {
     const file = "/opt/talend/secrets/secrets.properties"
     const secret = fs.readFileSync(file, 'utf8');
-    res.status(200).send(`Boom, secret is : ${secret}`);
+    const secretHtml = `<br>${secret.replace(/(?:\r\n|\r|\n)/g, '<br>')}`;
+    res.status(200).send(`Boom, secret is : ${secretHtml}`);
 });
 
 app.listen(PORT, function() {
