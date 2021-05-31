959a45874d885d80aeac9643473bcc3683e875ca
Chris Gruel <chris.gruel@armory.io>
2021-05-31 07:05:24 -0400

    mark unused variables

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var app = express();
 
 var PORT = process.env.PORT || 3000;
 
-app.get('/', function(req, res) {
+app.get('/', function(_req, res) {
     res.status(200).send('Hello world!!!!');
 });
 
-app.get('/accesskey', (req, res) => {
+app.get('/accesskey', (_req, res) => {
     const file = "/opt/talend/secrets/secrets.properties"
     const secret = fs.readFileSync(file, 'utf8');
     res.status(200).send(`Boom, secret is : ${secret}`);
