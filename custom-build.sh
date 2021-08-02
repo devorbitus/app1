84cf69979953438ede4a609b7dcd877fc41bbe49
Chris Gruel <cgruel@gmail.com>
2021-08-02 11:23:01 -0400

    nginx ingress and akeyless

---
+++ custom-build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker buildx build --platform linux/amd64,linux/arm64 --push -t "$IMAGE" "$BUILD_CONTEXT"
--- server.js
+++ server.js
@@ -6,13 +6,14 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.get('/', function(_req, res) {
-    res.status(200).send('Hello world!');
+    res.status(200).send('<h1>Hello world!</h1><br/><br/><a href="/accesskey">Navigate to Access Key Endpoint</a>');
 });
 
 app.get('/accesskey', (_req, res) => {
-    const file = "/opt/talend/secrets/secrets.properties"
+    const file = "/akeyless/secrets/K8s/app1secret"
     const secret = fs.readFileSync(file, 'utf8');
-    const secretHtml = `<br>${secret.replace(/(?:\r\n|\r|\n)/g, '<br>')}`;
+    // const secret = process.env.MY_SECRET;
+    const secretHtml = `<h1><br>${secret.replace(/(?:\r\n|\r|\n)/g, '<br>')}</h1>`;
     res.status(200).send(`Boom, secret is : ${secretHtml}`);
 });
 
